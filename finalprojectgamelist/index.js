2df5751f1efc28a6b046f26e12f3f62c8e2e40cd
alexeiASC <a0tulloch1@gmail.com>
2017-08-22 00:24:22 -0400

    smm

--- finalprojectgamelist/index.js
+++ finalprojectgamelist/index.js
@@ -1,1 +1,36 @@
-var rownum=1;var database=firebase.database().ref();function updateDB(){var img=$("#piclink").val();var name=$("#name").val();var gamename=$("#gamename").val();var gamel=$("#gamelink").val();console.log(name+"'s game: "+gamename);var value={Pic:img,Name:name,Gamename:gamename,Gamelink:gamel};database.push(value);}database.on("child_added",function(rowData){var row=rowData.val();var img=row.Pic;var name=row.Name;var gamename=row.Gamename;var gamel=row.Gamelink;var gamelist="<a href='"+gamel+"'<p>"+gamename+"  by "+name+" has "+" votes </p></a>";console.log(gamelist);var text="<div class='col-md-4'>"+"<div class='col-md-12'><img src="+img+" style='width:300px;height:200px;'></div><div class='col-md-12'><a href='"+gamel+"'<p>"+gamename+" by "+name+"</p></a></div></div>";$("#gamelist").append(text);})
+// 
+var rownum = 1;
+var database = firebase.database().ref();
+//button executes this function
+    function updateDB() {
+            var img = $("#piclink").val();
+            var name = $("#name").val();
+            var gamename = $("#gamename").val();
+            var gamel = $("#gamelink").val();
+        
+                console.log(name + "'s game: " + gamename);
+        
+            
+                //Update database here
+            var value = {
+     Pic:img,
+            Name: name,
+                Gamename: gamename,
+                    Gamelink: gamel
+                    };
+database.push(value);}
+
+    
+    database.on("child_added", function (rowData) {
+          var row = rowData.val();
+          var img = row.Pic;
+            var name = row.Name;
+            var gamename = row.Gamename;
+            var gamel = row.Gamelink;
+        var gamelist = "<a href='" + gamel + "'<p>" + gamename + "  by " + name + " has " + " votes </p></a>";
+        console.log(gamelist);
+            var text = "<div class='col-md-4'>" + "<div class='col-md-12'><img src=" + img + " style='width:300px;height:200px;'></div><div class='col-md-12'><a href='" + gamel + "'<p>" + gamename + " by " + name + "</p></a></div></div>";
+        $("#gamelist").append(text);
+        
+            
+            });
